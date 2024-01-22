507f4dfbadcc2d9a047fa64bde0345c68ecaa4d9
Diego Alves Teixeira <diegoteialves@gmail.com>
2024-01-22 14:51:16 -0300

    Criando um sistema de cadastro de alunos

--- day13/index.js
+++ day13/index.js
@@ -9,4 +9,23 @@ class Aluno {
     }
 }
 
-let aluno = new Aluno("Diego", 18)
+console.log("Cadastro de Alunos")
+let alunos = []
+let continuar = true
+let indexAlunos = 0
+
+while(continuar){
+    let nome = prompt("Informe o nome do aluno")
+    let idade = parseInt(prompt("Informe a idade do Aluno"))
+    let aluno = new Aluno(nome, idade)
+    alunos[indexAlunos] = aluno
+    indexAlunos++
+
+    let desejaContinuar = prompt("Deseja continuar? s ou n")
+
+    if(desejaContinuar == "s"){
+        continuar = true
+    }else{
+        continuar = false
+    }
+}
